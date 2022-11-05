9b7c6faf415f8b31ac144b9f0793f381433e2487
Olimpo Macea <olimpo3m@gmail.com>
2022-11-05 10:03:03 -0500

    ajustes finales v4

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -7,14 +7,14 @@
         span
           i.fas.fa-info
       h1 Introducción 
-    .tarjeta.p-4.p-md-5.mb-5.fondo1.color-gris(data-aos="fade-left")
+    .tarjeta.p-4.p-md-5.mb-5.fondo3(data-aos="fade-left")
       .row.justify-content-end.align-items-center
         .col-md-5.order-md-first.order-sm-last
           p.py-5 En el desarrollo de esta primera parte del proceso de fabricación digital de mobiliario, el aprendiz adquirirá las herramientas y habilidades básicas para llevar la generación de ideas de productos mobiliarios a propuestas plasmadas en bocetos o <em>sketchs</em> rápidos a mano, hasta recreaciones técnicas de productos para muestra y exposición a clientes y mercados potenciales. Para ello, tendrá en cuenta las características y requerimientos esenciales en un proceso de producción. Lo anterior se logrará comprendiendo la relación entre el diseño, la arquitectura, la ergonomía, tipos de clientes y varios métodos de marketing y de ventas de productos mobiliarios. 
 
         .col-md-6.mb-4.mb-md-0.order-md-last.order-sm-first
           figure
-            img(src="@/assets/curso/introduccion/principal.png")
+            img(src="@/assets/curso/introduccion/img-1.png")
 
     .titulo-segundo.color-acento-contenido.pb-2
       h2 Diseño de mobiliario
